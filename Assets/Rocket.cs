f211b448bf8bb52be1dfaf4478dd481664bf613d
digaspirate <diogocosta_slb@hotmail.com>
2020-09-15 15:08:27 +0100

    update

---
+++ Assets/Rocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = new Vector3(0, 0.1f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space Pressed");
+            transform.position += move;
+        }
+    }
+}
