8eb2efd55d2e2d5bbec1fb1dc6ca2eaa91a93c5d
digaspirate <diogocosta_slb@hotmail.com>
2020-09-15 20:10:40 +0100

    Thrusting and Rotating Speeds

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,41 +4,67 @@ public class Rocket : MonoBehaviour
 {
     Vector3 move;
     Rigidbody rigidBody;
+    AudioSource audiosource;
+    public float RotationSpeedMultiplier;
+    public float ThrustingSpeedMultiplier;
+    bool hasCollided;
 
     // Start is called before the first frame update
     void Start()
     {
+        hasCollided = false;
         move = new Vector3(0, 1f, 0);
         rigidBody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Rotate()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            print("Thrusting");
-            rigidBody.AddRelativeForce(0,1,0);
-        }
+
+        rigidBody.angularVelocity = Vector3.zero;      // nao esquecer desligar quando o collider bate
+        Vector3 tmp = transform.localEulerAngles;      //
+        tmp.x = 0; tmp.y = 0;                          // bloquear rotação eixos X e Y
+        transform.localEulerAngles = tmp;              //
+
+        float RotationSpeed = Time.deltaTime * RotationSpeedMultiplier;
+
         if (Input.GetKey(KeyCode.D) && (Input.GetKey(KeyCode.A)))
         {
             print("You cant rotate both ways");
-
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0,0,0.7f);
-
+            transform.Rotate(0,0, RotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0,0,-0.7f);
+            transform.Rotate(0, 0, -RotationSpeed);
+        }
+    }
 
+    private void Thrust()
+    {
+        float ThrustingSpeed = Time.deltaTime * 10 * ThrustingSpeedMultiplier;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting" + ThrustingSpeed);
+            rigidBody.AddRelativeForce(0, ThrustingSpeed, 0);
+            if (audiosource.isPlaying == false) //So it doesn't repeat
+            {
+                audiosource.Play();
+            }
+        }
+        else
+        {
+            audiosource.Stop();
         }
     }
 }
